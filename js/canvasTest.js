711f6257ee6b2cd9e4c3bf94da80339b74a6e897
Luiz Gustavo Santos Cirilo <lgcirilo@gmail.com>
2019-02-08 16:34:01 -0200

    Add files via upload
    Repository start

---
+++ js/canvasTest.js
@@ -0,0 +1,23 @@
+/** Canvas testing sandbox */
+
+const snakeCanvas = document.getElementById('snake');
+
+// keypress event is not fired for all keys (e.g. alt, ctrl, shift, esc, arrow keys) 
+document.addEventListener('keydown', (value) => console.log(value));
+
+// ctx is abbreviation for context
+// let ctx = snakeCanvas.getContext('2d');
+const ctx = snakeCanvas.getContext('2d');
+ctx.fillStyle = 'black';
+ctx.rect(10, 15, 200, 200);
+ctx.fill();
+ctx.closePath();
+ctx.beginPath();
+ctx.rect(10, 225, snakeCanvas.width - 20, 150);
+ctx.fillStyle = 'green';
+ctx.fill();
+
+setTimeout(() => {
+    ctx.clearRect(10, 15, 200, 200);
+    ctx.clearRect(10, 225, snakeCanvas.width - 20, 150);
+}, 500);

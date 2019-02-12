a245517822c5f1b526901c4325e891a17e4ad8fe
Luiz Gustavo Santos Cirilo <lgcirilo@gmail.com>
2019-02-12 13:33:37 -0300

    Add files via upload
    - Square can move in up, right, down and left when corresponding arrow key is pressed. No collision detection.

--- js/canvasTest.js
+++ js/canvasTest.js
@@ -7,30 +7,19 @@ document.addEventListener('keydown', (value) => move(value.keyCode));
 
 const squareSide = 10;
 let squarePosition = [0,0];
-
 const ctx = snakeCanvas.getContext('2d');
 
 // color canvas background
 ctx.fillStyle = 'black';
 ctx.rect(0, 0, snakeCanvas.width, snakeCanvas.height);
 ctx.fill();
+
 // draw a green square at the top left hand corner of the canvas
 ctx.closePath();
 ctx.beginPath();
 ctx.rect(squarePosition[0], squarePosition[1], squareSide, squareSide);
 ctx.fillStyle = 'green';
 ctx.fill();
-// move square one unit to  the right after five seconds
-// setTimeout(() => {
-//     ctx.fillStyle = 'black';
-//     ctx.rect(0, 0, snakeCanvas.width, snakeCanvas.height);
-//     ctx.fill();
-//     ctx.closePath();
-//     ctx.beginPath();
-//     ctx.fillStyle = 'green';
-//     ctx.rect(10, 0, 10, 10);
-//     ctx.fill();
-// },2000);
 
 function eraseCanvas () {
     ctx.fillStyle = 'black';
@@ -40,22 +29,40 @@ function eraseCanvas () {
 }
 
 function move(direction) {
-    if (direction === 39) {
-        
+    // move left (subtract 10 from x-axis)
+    if (direction === 37) {
+        eraseCanvas();
         ctx.beginPath();
+        squarePosition[0] = squarePosition[0] - 10;
         ctx.fillStyle = 'green';
-        ctx.rect(10, 0, 10, 10);
+        ctx.rect(squarePosition[0], squarePosition[1], squareSide, squareSide);
         ctx.fill();
     }
-
-    
+    // move up (subtract 10 from y-axis)
+    if (direction === 38) {
+        eraseCanvas();
+        ctx.beginPath();
+        squarePosition[1] = squarePosition[1] - 10;
+        ctx.fillStyle = 'green';
+        ctx.rect(squarePosition[0], squarePosition[1], squareSide, squareSide);
+        ctx.fill();
+    }
+    // move right (add 10 to x-axis)
+    if (direction === 39) {
+        eraseCanvas();
+        ctx.beginPath();
+        squarePosition[0] = squarePosition[0] + 10;
+        ctx.fillStyle = 'green';
+        ctx.rect(squarePosition[0], squarePosition[1], squareSide, squareSide);
+        ctx.fill();
+    }
+    // move down (add 10 to y-axis)
+    if (direction === 40) {
+        eraseCanvas();
+        ctx.beginPath();
+        squarePosition[1] = squarePosition[1] + 10;
+        ctx.fillStyle = 'green';
+        ctx.rect(squarePosition[0], squarePosition[1], squareSide, squareSide);
+        ctx.fill();
+    }
 };
-
-
-
-
-
-// setTimeout(() => {
-//     ctx.clearRect(10, 15, 200, 200);
-//     ctx.clearRect(10, 225, snakeCanvas.width - 20, 150);
-// }, 500);
